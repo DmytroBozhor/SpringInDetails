18bfac324c8d87ce08c521c88482f5f41115f03e
DmytroBozhor <dimaastop1@gmail.com>
2023-11-29 16:17:48 +0100

    singleton scope

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.entity.Animal;
+import org.example.entity.Cat;
 import org.example.entity.Person;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,9 +9,12 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Person person = context.getBean("person", Person.class);
-        System.out.println(person);
-        person.getAnimal().voice();
+//        singleton - meaning all the code will share the bean
+//        such beans are created as soon as the Spring context is instantiated and initialized
+        Animal orangeCat = context.getBean("animal", Cat.class);
+        Animal blackCat = context.getBean("animal", Cat.class);
+
+        System.out.println(orangeCat==blackCat);
 
         context.close();
     }
