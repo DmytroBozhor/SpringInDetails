679cf956ad60751683aa9358f6af78af1b7ee52c
DmytroBozhor <dimaastop1@gmail.com>
2023-12-13 11:34:55 +0100

    JoinPoint added

--- src/main/java/org/aop/util/aspects/LoggingAspect.java
+++ src/main/java/org/aop/util/aspects/LoggingAspect.java
@@ -1,22 +1,37 @@
 package org.aop.util.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Component
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public void addBook())")
-    public void beforeAddBookAdvice() {
-        System.out.println("LoggingAspect: Start: addBook()");
+    @Pointcut(value = "execution(void add*(..))")
+    private void allAddMethodsPointcut() {
     }
 
-    @After("execution(public void addBook())")
-    public void afterAddBookAdvice() {
-        System.out.println("LoggingAspect: End: addBook()");
+    @Before("allAddMethodsPointcut()")
+    public void beforeAddBookAdvice(JoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        Optional<Object[]> args = Optional.ofNullable(joinPoint.getArgs());
+
+        System.out.println("LoggingAspect :: beforeAddBookAdvice :: methodInfo :: " + signature);
+        args.ifPresent(objects -> Arrays.asList(objects).forEach(System.out::println));
+    }
+
+    @After("allAddMethodsPointcut()")
+    public void afterAddBookAdvice(JoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        System.out.println("LoggingAspect :: afterAddBookAdvice :: methodInfo :: " + signature);
     }
 
 }
